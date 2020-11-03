3dd95a8f9482404890cb8b2be74a801c2f69879b
alekhya <alekhya.rudra@cybrilla.com>
2020-11-03 20:18:00 +0530

    Adds react config initializer

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,12 +1,6 @@
-const styles = {
-  margin: "1em",
-  padding: "1em",
-  border: "1px solid #ddd"
-};
-
 const Main = props => {
   return (
-    <div style={styles}>
+    <div className="main">
       <center>
         <h1>React RoR Blog</h1>
       </center>
